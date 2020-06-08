2d1b84a0fa0da3c8bc44d4b96fc35d1c974542dd
totoki_kei <totoki_kei@outlook.com>
2020-06-09 00:20:25 +0900

    hand-pump prototype

---
+++ scripts/hand-pump.js
@@ -0,0 +1,23 @@
+//const handPump = extendContent(LiquidBlock, "hand-pump", {
+const handPump = extendContent(LiquidTank, "hand-pump", {
+        tapped(tile, player){
+        //Log.info("tapped.");
+        var liquidDrop = tile.floor().liquidDrop;
+
+        if (tile.entity.cons.valid() && liquidDrop != null){
+            // var maxPump = Math.min(
+            //     liquidCapacity - tile.entity.liquids.total(),
+            //     1
+            // ) * tile.entity.efficiency();
+            var maxPump = 1;
+
+            tile.entity.liquids.add(liquidDrop, maxPump);
+            Effects.effect(Fx.dooropen, Tmp.c1.set(Color.valueOf("84f491")), tile.drawx(), tile.drawy(), tile.block().size);
+        }
+
+    },
+    canPlaceOn(tile){
+        return tile != null && tile.floor().liquidDrop != null;
+    }
+
+});
